ff906e340ebcb86b7ff1ca66a0d35996babd32c5
syampolskiy <syampolskiy@jazzros.com>
2017-09-15 11:19:55 +0300

    Animated

--- src/components/TimelapseButton.js
+++ src/components/TimelapseButton.js
@@ -3,107 +3,145 @@ import Svg,{
     Circle,
     G
 } from 'react-native-svg';
-import {View, TouchableOpacity, Text, Animated, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, Text, Animated} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Bar from './bar';
+import Pie from './Circle';
 
-EStyleSheet.build();
-
-class TimelapseButton extends Component{
-  constructor(props) {
-    super(props);
-
-    this.diameter = 50;
-    this.radius = this.diameter / 2;
-    this.strokeDasharray = 2 * Math.PI * this.radius;
-    this.strokeDashoffset = this.strokeDasharray;
-    this.state = {
-        startStopTimer: 0,
-        progress: new Animated.Value(this.strokeDashoffset),
-        roundHasBeenFinished: 0
-    };
-  }
-
-  countTime = () => {
-    if (this.state.startStopTimer){
-      // stop timer
-      this.setState({
-        startStopTimer: 0,
-        progress: new Animated.Value(this.strokeDashoffset),
-        roundHasBeenFinished: 0
-      });
-    } else{
-      var self = this;
-      // start timer
-      this.setState({
-        startStopTimer: 1,
-        progress: new Animated.Value(this.strokeDashoffset),
-        roundHasBeenFinished: 0
-      });
-
-      Animated.timing(this.state.progress, {
-        toValue: 40,
-        duration: this.props.seconds*1000
-      }).start(function(){
-        console.log(self.state.progress)
-      });
-
-    }
-  };
-
-  componentDidMount() {
-    var self = this;
-    Animated.timing(self.state.progress, {
-        toValue: 50,
-        duration: self.props.seconds*1000
-      }).start(function(){
-        console.log(self.state.progress)
-        console.log(self.state.progress._value)
-      });
-  }
-
-  render(){
-    const rotate = "-90";
-    const progressWidth = 3;
-    const width = this.diameter + progressWidth;
-    const circleCenterPosition = width / 2;
-    const strokeDashoffset = this.strokeDasharray;
-
-    const transform = {
-        translate: `${circleCenterPosition},${circleCenterPosition}`, rotate: `${rotate}`
-      };
+class TmlpsBtnWrapper extends Component {
+  render() {
+    var innerRadius = parseFloat(this.props.radius) - parseFloat(this.props.strokeWidth);
 
     return (
-       <Animated.View style={{backgroundColor: 'grey'}}>
-         <TouchableOpacity onPress={this.props.seconds ? () => this.countTime() : () => this.countTime()} style={[styles2.buttonStyles]}>
-            <Svg style={[styles2.buttonStyles]}>
-              <G transform={transform}>
-                <Circle cx={0} cy={0} r={this.radius} fill="none" stroke={this.state.roundHasBeenFinished ? '#FFF' : 'green'} strokeWidth={progressWidth} />
-                <Circle cx={0} cy={0} r={this.radius-5} fill="#fff" />
-                <Circle cx={0} cy={0} r={this.radius} fill="none" stroke="#E62B08" strokeWidth={progressWidth} strokeDasharray={"" + this.strokeDasharray} strokeDashoffset={"" + this.state.progress._value}/>
-                </G>
-            </Svg>
-         </TouchableOpacity>
-                <Text style={{color: "#000"}}>{this.state.progress._value}</Text>
-       </Animated.View>
-            // <Bar value={120}/>
+      <View style={styles.containerForPressingBtn}>
+        <Pie
+          radius={this.props.radius}
+          innerRadius={innerRadius}
+          series={[this.props.series]}
+          colors={[this.props.strokeColor]}
+          backgroundColor={this.props.strokeBackground} />
+        {this.props.children}
+      </View>
     )
   }
 }
 
-const styles2 = EStyleSheet.create({
+export default class TimelapseButton extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progress: new Animated.Value(0),
+      timelapseColor: true
+    }
+  }
+
+  countTime = () => {
+      // this.setState({
+      //   progress: new Animated.Value(0)
+      // });
+     Animated.timing(this.state.progress, {
+        toValue: 100,
+        duration: 5000
+     }).start(function(){
+        this.setState({
+          progress: new Animated.Value(0),
+          timelapseColor: !this.state.timelapseColor
+        });
+        // this.countTime();
+     });
+  }
+
+  render() {
+    const AnimatedTmlpsBtnWrapper = Animated.createAnimatedComponent(TmlpsBtnWrapper);
+    return (
+        <View style={ styles.containerTmlps }>
+          <AnimatedTmlpsBtnWrapper series={this.state.progress} strokeWidth={1} radius={30} strokeColor={this.state.timelapseColor ? '#E62B08' : '#fff'} strokeBackground={this.state.timelapseColor == 1 ? '#fff' : '#E62B08'}>
+            <TouchableOpacity
+              onPress={this.countTime} style={[styles.buttonTmlpsStyles, {backgroundColor: '#fff'}]}
+            />
+          </AnimatedTmlpsBtnWrapper>
+        </View>
+
+          // <View style={styles.buttonContainerStyles}>
+          // </View>
+    );
+  }
+}
+
+const styles = EStyleSheet.create({
+  containerTmlps:{
+    backgroundColor: 'rgba(28, 42, 57, .7)'
+  },
+  containerForPressingBtn:{
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonTmlpsStyles: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    position: 'absolute'
+  },
+  container: {
+    // width: '11.56%',
+    backgroundColor: 'rgba(28, 42, 57, .7)',
+    paddingTop: 5,
+    paddingBottom: 10,
+    // flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'space-between',
+    // alignItems: 'center'
+  },
+  modeContainerStyles: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginTop: 10,
+    marginBottom: 5
+  },
+  buttonContainerStyles: {
+    padding: 1,
+    width: 60,
+    height: 60,
+    borderWidth: 1,
+    borderColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center'
+    // marginTop: 30,
+    // marginBottom: 20
+  },
   buttonStyles: {
-    width: 53,
-    height: 53,
-    borderRadius: 53
+    width: 50,
+    height: 50,
+    borderRadius: 50
+  },
+  buttonPlayStyles: {
+    marginBottom: 25,
+    padding: 5
+  },
+  switchContainer: {
+    width: '100%',
+    alignItems: 'center'
   },
   '@media (min-width: 1024)': {
+    container: {
+      paddingTop: 20,
+      paddingBottom: 15
+    },
+    buttonContainerStyles: {
+      width: 100,
+      height: 100,
+      borderRadius: 100
+    },
     buttonStyles: {
-      width: 89,
-      height: 89,
-      borderRadius: 89
-    }
+      width: 86,
+      height: 86,
+      borderRadius: 86
+    },
   }
 });
 
-export default TimelapseButton;
+EStyleSheet.build();
